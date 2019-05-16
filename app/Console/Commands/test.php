12d269fa6f7e8016b271932746838a3c007bf724
xin.guo <xin.guo@after615.com>
2019-05-16 16:14:25 +0800

    command & telescope

---
+++ app/Console/Commands/test.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'console:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'just for test';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $name = $this->ask("what your name?");
+        $secret = $this->secret("what is the password?");
+        Log::info($name . $secret);
+    }
+}
